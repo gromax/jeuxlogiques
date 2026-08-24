8f8aba0d39732ddcfd24294854411ca3b81be94e
gromax <gromaxx@gmail.com>
2026-08-24 20:48:13 +0200

    ajoute la correction du fichier de commande pour ne pas conserver une référence à un https

--- fromfile.py
+++ fromfile.py
@@ -44,7 +44,12 @@ with open(filename, "r", encoding="utf-8") as f:
     lines = f.readlines()
 
 content = ""
+corrected_lines = []
+correction_needed = False
 for line in lines:
+    corrected_lines.append(line)
+    if "#" in line:
+        line = line.split("#", 1)[0]  # Remove comments after '#'
     line = line.strip()
     if not line or line.startswith("#"):
         continue  # Ignore empty lines and comments
@@ -65,6 +70,14 @@ for line in lines:
             sys.exit(1)
         add_content = HABILLAGE.replace("--LABEL--", parts[0]).replace("--content--", result.stdout.strip())
         content += "\n" + add_content
+        # on veur rectifier le https par un id type tatham si possible
+        if len(parts) > 1 and parts[1].startswith("https") and "%id=" in result.stdout:
+            correction_needed = True
+            id_part = result.stdout.split("%id=", 1)[1].splitlines()[0].strip()
+            if id_part.endswith("%"):
+                # dans le tex on ajoute des % en fin de ligne
+                id_part = id_part[:-1].strip()
+            corrected_lines[-1] = f"{parts[0]} {id_part} #{parts[1]}\n"
 
 base_tex = base_tex.replace("--CONTENT--", content)
 
@@ -72,4 +85,9 @@ outfilename = filename.replace(".txt", ".tex") if filename.endswith(".txt") else
 
 with open(outfilename, "w", encoding="utf-8") as f:
     f.write(base_tex)
+
+# réécriture du fichier source avec les corrections
+if correction_needed:
+    with open(filename, "w", encoding="utf-8") as f:
+        f.writelines(corrected_lines)
 
