a7159ed6dccc08feb3a06b95ed48b70cdee8bc99
gromax <gromaxx@gmail.com>
2026-08-26 19:09:36 +0200

    réordonné + ajout pearl

--- main.py
+++ main.py
@@ -2,82 +2,50 @@ import sys
 
 if len(sys.argv) <= 2:
     print("Vous disposez des commandes :")
-    print("kenken <-- code -->")
-    print("bridges <-- code -->")
-    print("slant <-- code -->")
-    print('unequal "<-- code -->"')
     print('adjacent "<-- code -->"')
-    print('towers "<-- code -->"')
-    print('loopy <-- code -->')
-    print('palisade <-- code -->')
-    print('galaxy <-- code -->')
-    print('yingyang "<-- url ou code -->"')
-    print('nurikabe "<-- url ou code -->"')
-    print('undead "<-- code -->"')
-    print('techtonic "<-- code -->"')
+    print("bridges <-- code -->")
     print('camping "<-- code -->"')
+    print('galaxy <-- code -->')
+    print("kenken <-- code -->")
+    print('loopy <-- code -->')
+    print('nurikabe "<-- url ou code -->"')
+    print('palisade <-- code -->')
+    print('pearl <-- code -->')
+    print("slant <-- code -->")
+    print('techtonic "<-- code -->"')
     print('thermometres "<-- url ou code -->"')
+    print('towers "<-- code -->"')
+    print('undead "<-- code -->"')
+    print('unequal "<-- code -->"')
+    print('yingyang "<-- url ou code -->"')
+    print('--------------')
     print('grid <-- filename -->')
     sys.exit(1)
 
 com = sys.argv[1]
-if com == "kenken":
-    from modules.jeux.kenken import Kenken
-    k = Kenken(tatham = sys.argv[2])
-    print(k.tex())
+if com == "adjacent":
+    from modules.jeux.adjacent import Adjacent
+    a = Adjacent(tatham=sys.argv[2])
+    print(a.tex())
 elif com == "bridges" or com == "ponts":
     from modules.jeux.bridges import Bridges
     j = Bridges(tatham = sys.argv[2])
     print(j.tex())
-elif com == "slant":
-    from modules.jeux.slant import Slant
-    s = Slant(tatham = sys.argv[2])
-    print(s.tex())
-elif com == "unequal":
-    from modules.jeux.unequal import Unequal
-    u = Unequal(tatham = sys.argv[2])
-    print(u.tex())
-elif com == "adjacent":
-    from modules.jeux.adjacent import Adjacent
-    a = Adjacent(tatham=sys.argv[2])
-    print(a.tex())
-elif com == "towers":
-    from modules.jeux.towers import Towers
-    chaine = sys.argv[2]
-    t = Towers(tatham = chaine)
-    print(t.tex())
-elif com == "loopy":
-    from modules.jeux.loopy import Loopy
-    t = Loopy(tatham = sys.argv[2])
-    print(t.tex())
-elif com == "palisade":
-    from modules.jeux.palisade import Palisade
-    t = Palisade(tatham = sys.argv[2])
-    print(t.tex())
-elif com == "galaxy":
-    from modules.jeux.galaxy import Galaxy
-    g = Galaxy(tatham  = sys.argv[2])
-    print(g.tex())
-elif com == "yingyang":
-    from modules.jeux.yingyang import Yingyang
-    chaine = sys.argv[2]
-    if chaine.startswith("html"):
-        url = chaine.split(":",1)[1].strip() if ":" in chaine else ""
-        grid = Yingyang(url=url)
-    else:
-        grid = Yingyang(tatham=chaine)
-    print(grid.tex())
-elif com == "undead":
-    from modules.jeux.undead import Undead
-    u = Undead(tatham=sys.argv[2])
-    print(u.tex())
 elif com == "camping":
     from modules.jeux.camping import Camping
     grid = Camping(tatham=sys.argv[2])
     print(grid.tex())
-elif com == "techtonic":
-    from modules.jeux.techtonic import Techtonic
-    t = Techtonic(tatham=sys.argv[2])
+elif com == "galaxy":
+    from modules.jeux.galaxy import Galaxy
+    g = Galaxy(tatham  = sys.argv[2])
+    print(g.tex())
+elif com == "kenken":
+    from modules.jeux.kenken import Kenken
+    k = Kenken(tatham = sys.argv[2])
+    print(k.tex())
+elif com == "loopy":
+    from modules.jeux.loopy import Loopy
+    t = Loopy(tatham = sys.argv[2])
     print(t.tex())
 elif com == "nurikabe":
     from modules.jeux.nurikabe import Nurikabe
@@ -88,6 +56,22 @@ elif com == "nurikabe":
     else:
         grid = Nurikabe(tatham=chaine)
     print(grid.tex())
+elif com == "palisade":
+    from modules.jeux.palisade import Palisade
+    grid = Palisade(tatham = sys.argv[2])
+    print(grid.tex())
+elif com == "pearl":
+    from modules.jeux.pearl import Game
+    grid = Game(tatham = sys.argv[2])
+    print(grid.tex())
+elif com == "slant":
+    from modules.jeux.slant import Slant
+    s = Slant(tatham = sys.argv[2])
+    print(s.tex())
+elif com == "techtonic":
+    from modules.jeux.techtonic import Techtonic
+    t = Techtonic(tatham=sys.argv[2])
+    print(t.tex())
 elif com == "thermometres":
     from modules.jeux.thermometres import Thermometres
     chaine = sys.argv[2]
@@ -96,6 +80,28 @@ elif com == "thermometres":
         grid = Thermometres(url=url)
     else:
         grid = Thermometres(tatham=chaine)
+    print(grid.tex())
+elif com == "towers":
+    from modules.jeux.towers import Towers
+    chaine = sys.argv[2]
+    t = Towers(tatham = chaine)
+    print(t.tex())
+elif com == "undead":
+    from modules.jeux.undead import Undead
+    u = Undead(tatham=sys.argv[2])
+    print(u.tex())
+elif com == "unequal":
+    from modules.jeux.unequal import Unequal
+    u = Unequal(tatham = sys.argv[2])
+    print(u.tex())
+elif com == "yingyang":
+    from modules.jeux.yingyang import Yingyang
+    chaine = sys.argv[2]
+    if chaine.startswith("html"):
+        url = chaine.split(":",1)[1].strip() if ":" in chaine else ""
+        grid = Yingyang(url=url)
+    else:
+        grid = Yingyang(tatham=chaine)
     print(grid.tex())
 elif com == "grid":
     from modules.primitives.zones import Zones
